f7ec1553d4f3d47c278fb59100424d10cd49c5e5
AndrewWu20 <102876216+AndrewWu20@users.noreply.github.com>
2024-02-01 15:30:47 -0500

    Delete CS 180 directory

--- CS 180/HW 1/src/Identifiable.java
+++
@@ -1,8 +0,0 @@
-public interface Identifiable {
-    /**
-     * Returns the name of this identifiable object.
-     *
-     * @return the name of this identifiable object
-     */
-    String getName();
-}
--- CS 180/Project 5/Github-ServerCopy/README.md
+++
@@ -1,2 +0,0 @@
-# CS180Project5
-deez balls
